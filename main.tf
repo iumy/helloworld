4d322a749d1443b000b45729e9e57444f65afd27
Gianluca Iumiento <giumiento@prostrategy.ie>
2025-12-17 20:11:51 +0000

    fix

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ variable "my_md5_var" {
   type = string
 }
 
-resource "example_resource" "example" {
-  name = "resource-${var.my_md5_var}"
+# This is a built-in resource that just stores data in your state file
+resource "terraform_data" "example" {
+  input = var.my_md5_var
+}
+
+output "md5_result" {
+  value = terraform_data.example.output
 }
